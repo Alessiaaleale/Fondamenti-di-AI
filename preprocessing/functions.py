a87ea0c2a781a5677facf6db9e68ae134ebb5607
__aleale__ <drc.alessia@gmail.com>
2025-01-30 19:14:07 +0100

    Aggiunti metodi alla classe DataPreprocessing per permettere l'apertura dei file con estensione csv, xlsx, txt, json e tsv

--- preprocessing/functions.py
+++ preprocessing/functions.py
@@ -11,11 +11,31 @@ class DataPreprocessing:
         Rimuove le righe con valori NaN nella colonna target.
         """
         self.df = self.df.dropna(subset=[col_name])
+    def factorize_target_column(self, col_name) -> pd.DataFrame:
+        """
+        Sostituisce i valori della colonna target con valori numerici.
+        """
+        self.df[col_name] = pd.factorize(self.df[col_name])[0]
     def remove_commas_to_float(self) -> pd.DataFrame:
         """
         Sostituisce le virgole con i punti e converte le colonne in float.
         """
         self.df = self.df.replace(',', '.', regex=True).astype(float)
+    def delete_columns_with_80_percent_non_numeric(self) -> pd.DataFrame:
+        """
+        Rimuove le colonne con più dell'80% di valori non numerici.
+        """
+        threshold = 0.8
+        for column in self.df.columns:
+            num_non_numeric = self.df[column].apply(lambda x: not pd.api.types.is_numeric_dtype(type(x))).sum()
+            if num_non_numeric / len(self.df) > threshold:
+                self.df.drop(column, axis=1, inplace=True)
+    def replace_string_with_nan(self) -> pd.DataFrame:
+        """
+        Sostituisce le stringhe con valori NaN.
+        """
+        self.df = self.df.apply(pd.to_numeric, errors='coerce')
+
     def replace_nan(self, method_fill_nan, col_name) -> pd.DataFrame:
         """
         Sostituisce i valori NaN con la media o la mediana.
@@ -48,7 +68,10 @@ class DataPreprocessing:
         Esegue il preprocessing dei dati.
         """
         df = self.drop_nan_target(col_name)
+        df = self.factorize_target_column(col_name)
+        df = self.delete_columns_with_80_percent_non_numeric()
         df = self.remove_commas_to_float()
+        df = self.replace_string_with_nan()
         df = self.replace_nan(method_fill_nan, col_name)
         df = self.scale_columns()
         df = self.df
