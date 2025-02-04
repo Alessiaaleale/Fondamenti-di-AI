a73ab9fa3ef339a3ff6e7a40daeae52708cd7cc7
__aleale__ <drc.alessia@gmail.com>
2025-02-04 18:45:50 +0100

    Aggiornati main e requirements.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from evaluation.split import Split
 from metrics_results.metrics import MetricsCalculator
 from preprocessing.functions import DataPreprocessing
 from model.utility import classification_evaluation
+import os
 
 if __name__ == "__main__":
     file = input('Inserisci il nome del file con estensione: ')
@@ -10,8 +11,11 @@ if __name__ == "__main__":
     index_col = input('Inserisci il nome della colonna da impostare come indice (se presente): ')
     method_fill_nan = input('Scegli come fill nan values (mean or median): ')
 
+    csv_directory = "data"
+    file_path = os.path.join(csv_directory, file)
+
     file_opener = FileOpener()
-    df = file_opener.open(file)
+    df = file_opener.open(file_path)
 
     preprocessor = DataPreprocessing(df)
     df = preprocessor.preprocessing(index_col, target_column, method_fill_nan)
